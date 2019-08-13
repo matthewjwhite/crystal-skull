f0536abca2f12b8b029d4676dfb7a740713104a2
Matthew White <matthew.white.john@gmail.com>
2019-08-13 05:37:46 -0400

    Initial implementation of authentication system
    When creating user, pass base64-encoded key (base64-encoded to
    avoid newlines, so no need to call 'recv' multiple times).
        * Also made 'recv' calls attempt to get a max of 10000
          chars., for very large keys - though may be unnecessary...
            *** Look into ideal maximum upper bound, 'select', etc.
        * The decoded version is stored in the database.
    
    When you type in your name next time, it grabs the public key,
    encrypts a temporary secret, and then requests that you respond
    with the decrypted version of the secret. Note that the encrypted
    secret is base64-encoded, to avoid console rendering and/or
    copy-paste issues with the binary data.
    
    The command to perform the above should be as follows...
    echo '<FROM SERVER>' | base64 -d > challenge && openssl rsautl
    -decrypt -in challenge -inkey mykey.pem -out challenge-dec &&
    cat challenge-dec
    
    * This implementation works, but needs cleanup...

--- game/game.py
+++ game/game.py
@@ -3,6 +3,10 @@
 import pymongo
 import yaml
 import game.config as config
+import uuid
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+import base64
 
 CONFIG = config.Config()
 
@@ -19,6 +23,13 @@ class GameSession():
             msg = 'Does not exist! Create (Y/N)?'
             resp = self.socket.send_wait(msg)
             if resp.upper() == 'Y':
+                key = self.socket.send_wait('Paste your (base64-encoded) '
+                                            'public key.')
+                key = base64.b64decode(key).decode('utf-8')
+                if not key:
+                    self.socket.send('Bad key!')
+                    raise RuntimeError('Invalid key specified')
+
                 classes = CONFIG.get('player/class')
                 msg = 'Available classes: {}'.format(classes)
                 cls = self.socket.send_wait(msg)
@@ -26,8 +37,23 @@ class GameSession():
                     self.socket.send('Invalid class!')
                     raise RuntimeError('Invalid class selected')
 
-                self.user = {'name': name, 'class': cls}
+                self.user = {'name': name, 'class': cls,
+                             'auth': {'publicKey': key}}
                 self.db.user.insert_one(self.user)
+        else:
+            key = self.db.user.find_one({'name': name})['auth']['publicKey']
+            key = key.encode('utf-8')
+            key = RSA.importKey(key)
+            cipher = PKCS1_v1_5.new(key)
+            challenge = str(uuid.uuid4())
+            challenge_bytes = challenge.encode('utf-8')
+            challenge_bytes = cipher.encrypt(challenge_bytes)
+            enc = base64.b64encode(challenge_bytes).decode()
+            attempt = self.socket.send_wait(enc)
+            print(challenge)
+            print(attempt)
+            if challenge == attempt:
+                self.socket.send('woo!')
 
     def run(self):
         self._initialize()
@@ -37,7 +63,8 @@ class GameSocket():
         self._socket = socket
 
     def get(self):
-        return self._socket.recv(1024).decode('utf-8').strip()
+        # 10000 is arbitrary - general high number of bytes.
+        return self._socket.recv(10000).decode('utf-8').strip()
 
     def send(self, data):
         self._socket.sendall(data.encode('utf-8'))
