cdd1bdfb485ca2b68a13d518a30e732fcb4a2dc0
Matthew White <matthew.white.john@gmail.com>
2019-08-17 02:41:54 -0400

    Clean up user handling
    1. Move all core user handling and user creation/retrieval to a
    new 'user' module.
    2. Move authentication to 'user' module.
    3. Enforce a unique key on 'name' for 'user' from the beginning
    by putting the create index code at the beginning of the 'user'
    module.
        * This makes it so that duplicate (which we identify by having
        the same 'name') cannot be created. In other words, enforces
        another field to be unique, like '_id', though we don't have
        it set to autogenerate.
        * This will make it so that if multiple people are trying to
        create users with the same name at the same time (in which
        case there is a possibility that each thread will get beyond
        our existence check), Mongo will not reject all other creation
        requests but the first one.
        * https://docs.mongodb.com/manual/reference/method/db.collection.update/#use-unique-indexes

--- game/game.py
+++ game/game.py
@@ -1,62 +1,16 @@
 ''' Primary game module in game package, containing core game code '''
 
-import pymongo
-import yaml
-import game.config as config
-import uuid
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
-import base64
-
-CONFIG = config.Config()
+from game.user import User
 
 class GameSession():
 
     def __init__(self, socket):
         self.socket = GameSocket(socket)
-        self.db = pymongo.MongoClient('mongo', 27017).game
         self.user = None
 
-    def _initialize(self):
-        name = self.socket.send_wait('Welcome! User?')
-        if not self.db.user.find({'name': name}).count():
-            msg = 'Does not exist! Create (Y/N)?'
-            resp = self.socket.send_wait(msg)
-            if resp.upper() == 'Y':
-                key = self.socket.send_wait('Paste your (base64-encoded) '
-                                            'public key.')
-                key = base64.b64decode(key).decode('utf-8')
-                if not key:
-                    self.socket.send('Bad key!')
-                    raise RuntimeError('Invalid key specified')
-
-                classes = CONFIG.get('player/class')
-                msg = 'Available classes: {}'.format(classes)
-                cls = self.socket.send_wait(msg)
-                if cls not in classes:
-                    self.socket.send('Invalid class!')
-                    raise RuntimeError('Invalid class selected')
-
-                self.user = {'name': name, 'class': cls,
-                             'auth': {'publicKey': key}}
-                self.db.user.insert_one(self.user)
-        else:
-            key = self.db.user.find_one({'name': name})['auth']['publicKey']
-            key = key.encode('utf-8')
-            key = RSA.importKey(key)
-            cipher = PKCS1_v1_5.new(key)
-            challenge = str(uuid.uuid4())
-            challenge_bytes = challenge.encode('utf-8')
-            challenge_bytes = cipher.encrypt(challenge_bytes)
-            enc = base64.b64encode(challenge_bytes).decode()
-            attempt = self.socket.send_wait(enc)
-            print(challenge)
-            print(attempt)
-            if challenge == attempt:
-                self.socket.send('woo!')
-
     def run(self):
-        self._initialize()
+        self.user = User.get_user(self.socket)
+        self.socket.send('Welcome, {}!'.format(self.user.name))
 
 class GameSocket():
     def __init__(self, socket):
