3deb10b23373b0812ab924b583cfb9c3bf074f08
Matt White <matthew.white.john@gmail.com>
2019-12-12 21:23:24 -0500

    Get clean Pylint

--- game/config.py
+++ game/config.py
@@ -3,20 +3,24 @@
 import yaml
 
 class ConfigError(Exception):
-    pass
+    ''' Exception for configuration errors '''
 
-class Config():
+class Config(): # pylint: disable=too-few-public-methods
+    ''' In-memory representation of configuration on disk '''
+
     def __init__(self, loc='/game/config.yml'):
         with open(loc, 'r') as fil:
             self._config = yaml.load(fil, Loader=yaml.SafeLoader)
 
     def get(self, path):
+        ''' Used to query for config element '''
+
         data = self._config
         try:
             parts = path.split('/')
             for part in parts:
                 data = data[part]
-        except (KeyError, TypeError) as error:
+        except (KeyError, TypeError):
             raise ConfigError('Failed to get path: {}'.format(path))
 
         return data
--- game/game.py
+++ game/game.py
@@ -2,28 +2,39 @@
 
 from game.user import User
 
-class GameSession():
+class GameSession(): #pylint: disable=too-few-public-methods
+    ''' Represents the game session for a single player '''
 
     def __init__(self, socket):
         self.socket = GameSocket(socket)
         self.user = None
 
     def run(self):
+        ''' Kicked off when the Greenlet is created '''
+
         self.user = User.get_user(self.socket)
         self.socket.send('Welcome, {}!'.format(self.user.name))
 
 class GameSocket():
+    ''' Wrapper for the server socket, to communicate with client '''
+
     def __init__(self, socket):
         self._socket = socket
 
     def get(self):
+        ''' Gets data in buffer from client '''
+
         # 10000 is arbitrary - general high number of bytes.
         return self._socket.recv(10000).decode('utf-8').strip()
 
     def send(self, data):
+        ''' Sends data to client '''
+
         self._socket.sendall(data.encode('utf-8'))
 
     def send_wait(self, data):
+        ''' Sends and waits for data '''
+
         # Add trailing space to further indicate requested output.
         self.send('{} '.format(data))
         return self.get()
