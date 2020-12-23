d2702275bb96ab2b18d7f99e89837854a387532b
Matt White <matthew.white.john@gmail.com>
2020-12-23 04:38:02 -0500

    Validate final stuff that Yamale can't
    Implements #18

--- game/config.py
+++ game/config.py
@@ -20,18 +20,27 @@ class Config(): # pylint: disable=too-few-public-methods
         return cls._single
 
     def __init__(self, loc, schema_loc):
+        # Load configuration YAML and schema files.
         try:
             config = yamale.make_data(loc)
             schema = yamale.make_schema(schema_loc)
         except OSError as error:
             raise ConfigError('Unable to read file') from error
 
+        # Validate structure of YAML file.
         try:
             yamale.validate(schema, config)
         except ValueError as error:
             raise ConfigError('Configuration deviates from schema') from error
 
         self._config = config[0][0]
+
+        # Validate non-structural information.
+        monster_names = [ monster['name'] for monster in self._config['monster'] ]
+        for mp in self._config['map']: #pylint: disable=invalid-name
+            if [ monster_name for monster_name in mp['monsters'] \
+                     if monster_name not in monster_names ]:
+                raise ConfigError('One or more map-referenced monsters not defined')
 
     def get(self, path):
         ''' Used to query for config element '''
